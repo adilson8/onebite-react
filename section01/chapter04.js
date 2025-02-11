738901676c537a555ded64a9d847d8044b875658
adilson8 <adilson8@naver.com>
2025-02-11 13:03:00 +0900

    chapter04

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -1,1 +1,26 @@
-console.log("chapter 4");
+// 1. 변수
+let age;
+console.log(age);
+
+age = 30;
+console.log(age);
+
+// 2. 상수
+const birth  = "1997.01.07";
+
+// 3. 변수 명명 규칙 (네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다.
+let $_name = '홍길동';
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1;
+// let 1name;
+
+// 3-3. 예약어는 사용할 수 없다.
+// let let;
+// let if;
+
+// 4. 변수 명명 가이드 (직관적 변수명 사용)
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
